6900284f9f9fdc95647afdedcafbdc92039c8377
Fellipe Juncal <fjuncal7@gmail.com>
2025-02-10 22:17:11 -0300

    Implementando as rotas no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/quiz" element={<h1>Quiz</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
