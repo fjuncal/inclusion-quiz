dbf9fbb8dac6c9f2919779cafab672564713ec0f
Fellipe Juncal <fjuncal7@gmail.com>
2025-02-10 23:20:04 -0300

    Adicionando mais links na pagina de recursos

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -36,7 +36,73 @@ function Resources() {
             Artigo Sobre Tecnologia e Inclusão: importância e desafios
           </a>
         </li>
-        {/* Adicione mais recursos conforme desejar */}
+        <li>
+          <a
+            href="https://tiinside.com.br/12/05/2023/diversidade-ainda-e-desafio-no-setor-de-tecnologia-da-informacao-mas-empresas-estao-dispostas-a-mudar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diversidade ainda é desafio no setor de Tecnologia da Informação
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://totidiversidade.com.br/blog/diversidade-na-tecnologia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diversidade na tecnologia: cenário atual, demissões em massa e dicas
+            para promover inclusão
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.catho.com.br/carreira-sucesso/diversidade-na-tecnologia-quais-os-beneficios/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diversidade na tecnologia: confira os benefícios
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.flyeducacao.org/post/diversidade-no-mundo-da-tecnologia-um-mercado-de-trabalho-mais-inclusivo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diversidade no mundo da tecnologia: Um mercado de trabalho mais
+            inclusivo
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://projetogauss.org/2023/08/04/a-importancia-da-diversidade-e-inclusao-na-tecnologia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            A IMPORTÂNCIA DA DIVERSIDADE E INCLUSÃO NA TECNOLOGIA
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://codebit.com.br/blog/empresas/diversidade-tecnologia-descubra-qual-importancia-como-implementar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diversidade na tecnologia: descubra qual a importância e como
+            implementar
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://esr.rnp.br/rh/diversidade-e-inclusao-em-ti/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Diversidade e inclusão em TI: qual a importância de discutir essas
+            pautas antes da seleção de seu próximo time?
+          </a>
+        </li>
       </ul>
     </div>
   );
